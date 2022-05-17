808eb9b010bb829d7634b384c7993e36f5a72fe3
Josefina Figueroa <33705733+a-hognose-snake@users.noreply.github.com>
2022-05-16 23:48:58 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Arrow funtion (snippet: sfc)
+const  App = () => {
+  return ( 
+    <>
+      <h1>Bienvenido al testing sandbox</h1>
+    </>
+   );
+}
+ 
+export default App;
