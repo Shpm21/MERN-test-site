2335661513acc0ae2eaff36bbea130e5744cd856
Josefina Figueroa <33705733+a-hognose-snake@users.noreply.github.com>
2022-05-18 01:01:58 -0400

    react-router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductScreen from './screens/ProductScreen';
 
 
 
 // Arrow funtion (snippet: sfc)
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <HomeScreen></HomeScreen>
+          <h1>Hello, React Router!</h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
- 
+
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import { Navbar, Nav, Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from "react-router-bootstrap";
 
 //rafce : react arrow function component export (shortcut)
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg="light" variant="light" expand = 'lg' collapseOnSelect>
-            <Container>
-                <Navbar.Brand href='/'>Test Site</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='justify-content-end flex-grow-1 pe-3'>
-                        <Nav.Link href='/cart'><i className="fa-solid fa-cart-shopping"></i> Mis Compras </Nav.Link>
-                        <Nav.Link href='/login'><i className="fa-solid fa-user"></i> Inicia Sesión</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
+    return (
+        <header>
+            <Navbar bg="light" variant="light" expand='lg' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Test Site</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='justify-content-end flex-grow-1 pe-3'>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fa-solid fa-cart-shopping"></i> Mis Compras </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className="fa-solid fa-user"></i> Inicia Sesión</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
