6a33d54982dc4f6aa7f9a0d4c65bce9e2d82be4c
Josefina Figueroa <33705733+a-hognose-snake@users.noreply.github.com>
2022-07-13 17:47:45 -0400

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import { productListReducer } from './reducers/productReducers'
 const store = configureStore({
     reducer: { productList: productListReducer },
     preloadedState: {},
-    devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
+    devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store
